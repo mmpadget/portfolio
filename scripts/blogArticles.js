bc8d718fe1b22957217ef9c0cccdfc7338399c46
Michael Padget <mpadget@comcast.net>
2017-03-15 12:10:07 -0700

    Added project constructor and content template

---
+++ scripts/blogArticles.js
@@ -0,0 +1,12 @@
+// 'use strict';
+//
+// var rawData = [
+//   {
+//     imageUrl: '',
+//     caption: '',
+//     title: 'Project Title',
+//     projectUrl: '',
+//     subTitle: 'Javascript',
+//     description: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam faucibus elit facilisis dui vestibulum faucibus. Sed eget dictum ipsum. Maecenas.</p>'
+//   },
+// ];
