89183a8e314bf5d0ff39c6491e31e9378b5298a8
Michael Padget <mpadget@comcast.net>
2017-03-20 12:29:38 -0700

    Improved mobile CSS and icons

--- scripts/article.js
+++ scripts/article.js
@@ -2,7 +2,6 @@
 
 var allArticles = [];
 
-// Constructor function: save all properties of 'opts' into 'this'.
 function Article (opts) {
   this.title = opts.title;
   this.subTitle = opts.subTitle;
@@ -13,30 +12,16 @@ function Article (opts) {
   this.caption = opts.caption;
 }
 
-// Clone template on HTML page.
 Article.prototype.toHtml = function() {
-  // var $newArticle = $('article.template').clone();
-  //
-  // // Prevents dupicating of template multiple times.
-  // $newArticle.removeClass('template');
-  //
-  // // Edit each instance of copied template with unique data.
-  // $newArticle.find('h2').html(this.title);
-  // $newArticle.find('h3').html(this.subTitle);
-  // $newArticle.find('p.description').html(this.description);
-  // $newArticle.find('a.project-url').attr('href',this.projectUrl);
-  // $newArticle.find('a.published-url').attr('href',this.publishedUrl);
-  //
-  // return $newArticle;
-  var source = $('#articles-template').html();
 
+  var source = $('#articles-template').html();
+  // eslint-disable-next-line
   var templateRender = Handlebars.compile(source);
-  console.log(templateRender);
-  console.log(source);
+
   return templateRender(this);
 };
 
-// For all raw data, push into articles array.
+// eslint-disable-next-line
 rawData.forEach(function(articleObject) {
   allArticles.push(new Article(articleObject));
 });
--- scripts/articleView.js
+++ scripts/articleView.js
@@ -5,14 +5,10 @@ var articleView = {};
 // For any child element, click "this"
 articleView.handleMainNav = function () {
   $('.nav-links').on('click', '.tab', function() {
-    // TODO:
-    //   1. Hide all of the .tab-content sections
-    //   2. Fade in the single .tab-content section that is
-    //     associated with the .tab element's data-content attribute.
 
     // Show the content of that tab.
-    $('.tab-content').hide(); // Hide all.
-    $('#'+ $(this).data('content')).fadeIn(); // Another Concatenation.
+    $('.tab-content').hide();
+    $('#'+ $(this).data('content')).fadeIn();
   });
   $('.nav-links .tab:first').click();
 };
